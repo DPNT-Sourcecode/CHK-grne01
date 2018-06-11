6c7a16e8c030fd8e75bfa820740fbf4fe3f915f0
sbx_user1060 <sbx_user1060@ip-10-12-2-144.eu-west-2.compute.internal>
2018-06-11 11:45:59 +0000

    Mon Jun 11 11:45:59 UTC 2018

--- test/solutions/test_hello.py
+++ test/solutions/test_hello.py
@@ -4,22 +4,16 @@ import sys, os
 
 sys.path.insert(0, os.path.abspath("../.."))
 
-from lib.solutions.sum import sum
+from lib.solutions.hello import hello
 
 
-class TestSum(unittest.TestCase):
-    def test_sum(self):
-        self.assertEqual(sum(1, 2), 3)
+class TestHello(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(hello("world"), "Hello world")
 
-    def test_sum_pos(self):
-    	self.assertTrue(sum(1, 1) >= 0)
-
-    def test_sum_neg(self):
-    	self.assertTrue(sum(1, -11) < 0)
-
-    def test_sum_type(self):
+    def test_hello_type_error(self):
     	with self.assertRaises(TypeError):
-   			sum("a", 0)
+   			hello(9000)
 
 
 if __name__ == '__main__':
