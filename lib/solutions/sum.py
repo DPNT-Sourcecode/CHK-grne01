60c9dd9c7fa3bb3ed90573512bdf033e5359a98f
sbx_user1060 <sbx_user1060@ip-10-12-2-144.eu-west-2.compute.internal>
2018-06-11 11:35:07 +0000

    Mon Jun 11 11:35:07 UTC 2018

---
+++ lib/solutions/sum.py
@@ -0,0 +1,5 @@
+# noinspection PyShadowingBuiltins,PyUnusedLocal
+def sum(num1, num2):
+	if not isinstance(num1, int) or not isinstance(num2, int):
+		raise TypeError("One or both passed arguments is not an integer.")
+	return num1 + num2
---
+++ test/solutions/test_sum.py
@@ -0,0 +1,16 @@
+import unittest
+import sys, os
+
+
+sys.path.insert(0, os.path.abspath("../.."))
+
+from lib.solutions.sum import sum
+
+
+class TestSum(unittest.TestCase):
+    def test_sum(self):
+        self.assertEqual(sum(1, 2), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
