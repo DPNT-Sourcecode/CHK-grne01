ea36e397617ab0ec5344ea7d5af951b99cd2c78b
sbx_user1060 <sbx_user1060@ip-10-12-2-144.eu-west-2.compute.internal>
2018-06-11 12:33:55 +0000

    Mon Jun 11 12:33:55 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -12,11 +12,28 @@ def checkout(skus):
 
 	cart_value = 0
 	cart = list(skus)
+	
 	# Check basket and group items (to check for offers)
+	
+	def deal(sku, count):
+		if sku == "A":
+			remainder = count % 3
+			return (((count - remainder) / 3) * 130) + (remainder * 50)
+		elif sku == "B":
+			remainder = count % 2
+			return (((count - remainder) / 2) * 45) + (remainder * 30)
+		else:
+			return False
+			
+
 	for k,v in stock.items():
-		if k in skus:
-			cart_value += skus.count(k) * v
+		if k in cart:
+			deal_value = deal(k, cart.count(k))
+			if not deal_value:
+				cart_value += skus.count(k) * v
+			else:
+				cart_value += deal_value
 
 	return cart_value
 
-print checkout("AA")
+print checkout("AAAADDD")
---
+++ test/solutions/test_checkout.py
@@ -0,0 +1,21 @@
+import unittest
+import sys, os
+
+
+sys.path.insert(0, os.path.abspath("../.."))
+
+from lib.solutions.checkout import checkout
+
+
+class TestCheckout(unittest.TestCase):
+    def test_checkout(self):
+        self.assertEqual(checkout("AABC"), 150)
+
+    def test_checkout_a_deal(self):
+        self.assertEqual(checkout("AAABC"), 180)
+
+    def test_checkout_b_deal(self):
+        self.assertEqual(checkout("ABBC"), 115)
+
+if __name__ == '__main__':
+    unittest.main()
