370a266370e80f065a6ab94744f2f4946c2a9c37
sbx_user1059 <sbx_user1059@ip-10-12-240-5.eu-west-2.compute.internal>
2018-06-11 11:28:46 +0000

    Mon Jun 11 11:28:46 UTC 2018

--- lib/solutions/sum.py
+++
@@ -1,5 +0,0 @@
-# noinspection PyShadowingBuiltins,PyUnusedLocal
-def sum(num1, num2):
-	if not isinstance(num1, int) or not isinstance(num2, int):
-		raise TypeError("One or both passed arguments is not an integer.")
-	return num1 + num2
--- test/test_mod.py
+++
@@ -1,3 +0,0 @@
-from .lib.solutions.sum import sum
-
-print(sum(1,2))
