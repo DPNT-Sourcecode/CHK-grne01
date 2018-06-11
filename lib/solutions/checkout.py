2dbe92617837c0edc293b0a93dddcb49cc3cd05a
sbx_user1060 <sbx_user1060@ip-10-12-2-144.eu-west-2.compute.internal>
2018-06-11 12:00:55 +0000

    Mon Jun 11 12:00:55 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -3,4 +3,14 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+	stock = {
+		"A": 50,
+		"B": 30,
+		"C": 20,
+		"D": 15
+	}
+
+	# Check basket and group items (to check for offers)
+	return False
+
+checkout("AABBC")
